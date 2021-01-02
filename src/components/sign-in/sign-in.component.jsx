ea4ccf314aac9e1e2812604df44a8f9cdd40a44a
= <=>
2021-01-02 15:16:28 -0600

    adding signin with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {
-  signInWithGoogle,
-  SignInWithGoogle,
-} from "../../firebase/firebase.utils";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
