ab5d6f59fe3db86bb336d2fc35c23433b5baae89
= <=>
2021-01-20 21:42:04 -0600

    moving directory and shop data to its own reducer, created selectors and updated components with new products flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,13 @@
 import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectShop = (state) => state.shop;
 
@@ -16,10 +16,18 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collectoin) => collections.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) =>
+      // collections.find(
+      //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+      // )
+      collections[collectionUrlParam]
   )
 );
